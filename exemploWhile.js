62850887e416e76c712bbc7650e68e040c723729
dnycolas <144865380+dnycolas@users.noreply.github.com>
2024-05-07 17:20:19 -0300

    Add files via upload
    mais exercícios usando condições

---
+++ exemploWhile.js
@@ -0,0 +1,81 @@
+// for - loop contado
+
+// for (let cont= 10; cont >= 0; cont--) {
+//     console.log(cont)
+// }
+
+// for (let cont=1; cont>=; cont++) {
+//     console.log(cont)
+// }
+
+
+
+
+
+
+
+
+// while - loop condicional
+
+// let contador = 0; 
+
+// while (contador < 5) {
+//     console.log(contador);
+//     contador+=1
+// }
+
+
+// let opcao = prompt("digite uma opção\n")
+// while (opcao != "0"){
+//     console.log("Repetiuuuuuuuuu")
+// }
+//  console.log("saiuu")
+
+
+
+
+
+
+
+
+
+
+
+// do - while -> loop condicional
+
+// let contador = 0
+
+// do {
+//     console.log(contador)
+//     contador+=1
+// }while(contador <= 5)
+
+
+// let num1 = 0; 
+
+// do {
+
+//     console.log(num1);
+//     num1+=1
+// }while (num1 < 10)
+
+
+
+
+
+
+// forEach (para cada - percorrer a lista)
+
+
+
+
+// let num = [1,2,3,4,5,6,7,];
+
+// num.forEach(elemento => {
+//     console.log("Os número são: " + elemento)
+// });
+
+
+// for(let elemento of num) {
+//     console.log(elemento)
+// }
