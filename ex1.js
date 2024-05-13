128fd9838e963d52c64d0a826d743f00ebafcd96
dnycolas <dnycolas088@gmail.com>
2024-05-13 11:52:30 -0300

    feat: exercicios de função e objeto feitos

--- ex1.js
+++
@@ -1,1 +0,0 @@
-// 1. Imprimir números de 1 a 10 utilizando o loop for
--- while1.js
+++
@@ -1,9 +0,0 @@
-// 6. Imprimir números pares de 0 a 20 utilizando o loop while
-
-
-let cont = 0
-
-while (cont <= 20) {
-    console.log(cont)
-    cont++
-}
